acd93ea979593f756cab817b82b92ab54dbd40e0
aldojesva <aldojesva@gmail.com>
2024-08-23 19:45:21 -0600

    login-back-end

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index'])->name('auth\home');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/logados', [AuthController::class, 'logados'])->name('logados');
