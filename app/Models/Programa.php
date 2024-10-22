0ffef09785dca1c8700925613986cf0ae442cd33
aldojesva <aldojesva@gmail.com>
2024-10-22 09:08:19 -0600

    cambios

--- app/Models/Programa.php
+++ app/Models/Programa.php
@@ -6,27 +6,44 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Programa extends Model
+class Modalidad extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $table = 'modalidades'; // Nombre de la tabla
+
     protected $fillable = [
-        'nombre',
-        'descripcion',
+        'titulo',
+        'subtitulo',
+        'titulo_perfil_ingreso',
+        'desc_perfil_ingreso',
+        'link_img_perfil_ingreso',
+        'titulo_perfil_egreso',
+        'desc_perfil_egreso',
+        'link_img_perfil_egreso',
+        'titulo_mapa_curricular',
+        'desc_mapa_curricular',
+        'link_img_mapa_curricular',
+        'link_video_clase_muestra',
+        'link_img_extra',
+        'desc_extra',
     ];
+
+    protected $dates = ['deleted_at'];
+
+    // Mutador para asegurar que el título siempre tenga la letra inicial en mayúscula
+    public function setTituloAttribute($value)
+    {
+        $this->attributes['titulo'] = ucfirst($value);
+    }
 
     public function servicios()
     {
-        return $this->belongsToMany(Servicio::class, 'programa_servicio');
-    }
-    
-    public function modalidades()
-    {
-        return $this->belongsToMany(Modalidad::class, 'modalidad_programa');
+        return $this->belongsToMany(Servicio::class, 'servicio_modalidad');
     }
 
-    public function aspirantes()
+    public function programas()
     {
-        return $this->belongsToMany(Aspirante::class, 'aspirante_programa');
+        return $this->belongsToMany(Programa::class, 'modalidad_programa');
     }
 }
